83de881acb014c8e269eae568b1deec9eaa090a6
Xavier <xyounan@outlook.com>
2020-09-05 14:50:29 +1000

    sculpture

--- Australian Maths Trust Website/AIO Senior 2005/suculpture/main.cpp
+++ Australian Maths Trust Website/AIO Senior 2005/suculpture/main.cpp
@@ -1,116 +1,64 @@
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-/*
-void dfs(int s, int e){
-    for(auto u: adj[s]){
-        if(u!=e) dfs(u,s)
-    }
+
+
+int n;
+int total = 0;
+bool leaf[50005];
+int lb[50005][2];
+int rb[50005][2];
+
+int a,x,b,y;
+
+int dfs(int v){
+    if(leaf[v]) return 0;
+
+    int leftHeight = dfs(lb[v][0]) + lb[v][1];
+    int rightHeight = dfs(rb[v][0]) + rb[v][1];
+
+    total += max(leftHeight,rightHeight) - min(leftHeight,rightHeight);
+    //  total += abs(leftHeight - rightHeight);
+
+    return max(leftHeight,rightHeight);
+
 }
-*/
+
+
 
 int main()
 {
+
+
+
+
     ifstream inputFile("sculpin.txt");
     ofstream outputFile("sculpout.txt");
 
 
-    int n;
     inputFile >> n;
 
-
-    int apple[n+1][6] = {{0,0,0,0,-1,0}};
-    int tier[n] = {0};
-    int maxTier = 0;
-
-
-    int a,x,b,y;
-
-    //Process
-    for(int i = 1; i<n+1; i++){
-
-
+    for(int i=1; i<=n; i++){
         inputFile >> a >> x >> b >> y;
 
-        apple[i][0] = a;
-        apple[i][1] = x;
-        apple[i][2] = b;
-        apple[i][3] = y;
-        //cout << "Height is: " << apple[i][4] << endl;
-        if( a!= 0){
-            apple[a][4] = i;
-            apple[a][5] = x;
-
-
-
-            tier[a] = tier[i] + 1;
-            tier[b] = tier[i] + 1;
-
-            maxTier = max(maxTier, tier[a]);
-            maxTier = max(maxTier, tier[b]);
-
-            apple[b][4] = i;
-            apple[b][5] = y;
-
-
-        }
-    }
-
-
-    int maxHeight = 0;
-    int heights[n] = {0};
-    int subBranchId, height;
-
-    for(int i=1; i<=n; i++){
-        subBranchId = apple[i][4];
-        height = apple[i][5];
-
-
-        while(subBranchId != -1){
-            height += apple[subBranchId][5];
-            subBranchId= apple[subBranchId][4];
+        if(a == x && x == b && b == y && y==0){
+            leaf[i] = true;
+            continue;
         }
 
-        heights[i] = height;
-        maxHeight = maxHeight>height ? maxHeight : height;
+        leaf[i] = false;
+        lb[i][0] = a;
+        lb[i][1] = x;
+
+        rb[i][0] = b;
+        rb[i][1] = y;
     }
 
+    dfs(1);
 
-    int growAmount[n+1] = {0};
-
-
-
-
-    cout << "Method 1" << endl;
-    for(int i=maxTier; i>=0; i--){
-        for(int j=1; j<n+1; j++){
-            if(tier[j] == i){
-                cout << "Apple is: " << j << " Tier: " << i << endl;
-                if(apple[j][0] == 0){
-                    growAmount[j] += maxHeight - heights[j];
-
-                }else{
-
-                    growAmount[j] += min(growAmount[apple[j][0]], growAmount[apple[j][2]]);
-
-                    growAmount[apple[j][0]] -= growAmount[j];
-                    growAmount[apple[j][2]] -= growAmount[j];
-                }
-            }
-        }
-    }
-
-
-    int totalGrow = 0;
-    for(int i=1; i<=n; i++){
-        totalGrow += growAmount[i];
-
-    }
-
-
-    outputFile << totalGrow ;
+    outputFile << total ;
     outputFile.close();
     return 0;
 }
-
